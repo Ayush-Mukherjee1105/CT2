35add545e2b58240a3c7d9e4793ae05603189f5f
Ayush Mukherjee <ayushmukherjee2017@gmail.com>
2025-05-02 04:17:08 +0530

    update

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -8,7 +8,8 @@ const Home = () => {
       <div className="home-card">
         <h1>Welcome to <span className="highlight">Team APEX</span> 🎓</h1>
         <p className="home-text">
-          Welcome to the student team members management appication!! Add, view, and update members with ease.
+          Welcome to the student team members management appication!!
+          Add, view, and update members with ease.
         </p>
         <div className="home-actions">
           <Link to="/add">
